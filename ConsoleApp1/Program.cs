0d020d71765fed27963b79325032fa2b44318331
Nata <ganenkova@mail.ru>
2023-03-14 19:27:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+Clear();
+Write("Введите значения через пробел: ");
+string[] array = ReadLine().Split(" ");
+int count = 0;
+
+
+ReadLine();
+
+
+
